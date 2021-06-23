ca3214397e4232200ec7c75758b80925e7c1c72a
OH-JOO-YEONG <brb1111@naver.com>
2021-06-23 23:48:32 +0900

    feat: movie Service part done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
   controllers: [MoviesController], // NodeJS의 라우터 역할
-  providers: [],
+  providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,39 +6,36 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
-
-  @Get('search')
-  search(@Query('year') serachingYear: string) {
-    return `We are searching for a movie with made after: ${serachingYear}`;
-  }
-
-  @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return One movie with the id: ${movieId}`;
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
-    return { updatedMovie: movieId, ...updateData };
+    return this.moviesService.update(movieId, updateData);
   }
 }
