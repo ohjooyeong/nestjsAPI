c0166164fa08311f18f966e2fc97af0864395caa
OH-JOO-YEONG <brb1111@naver.com>
2021-06-28 23:12:44 +0900

    refactor: Module and Dependency Injection structure change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], // NodeJS의 라우터 역할
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // NodeJS의 라우터 역할
+  providers: [],
 })
 export class AppModule {}
